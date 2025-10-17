c46ed2f9a4b70036332a0161120b4c6a740e61f4
VietBinh17 <154102208+binh12vv@users.noreply.github.com>
2025-10-17 10:09:32 +0700

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -17,13 +17,30 @@
  */
 
 #include <stdint.h>
+#include <timer.h>
+#include <ucglib.h>
 
-#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-#endif
+static ucg_t ucg;
 
 int main(void)
 {
+    SystemCoreClockUpdate(); // cấp clock
+    TimerInit();             // để khởi tạo chức năng timer.
+
+    // Khởi tạo LCD
+    Ucglib4WireSWSPI_begin(&ucg, UCG_FONT_MODE_SOLID);
+    ucg_ClearScreen(&ucg);
+
+    ucg_SetFont(&ucg, ucg_font_helvR08_tf);    // lựa chọn font chữ và cỡ chữ
+    ucg_SetColor(&ucg, 0, 255, 255, 255);      // chọn màu chữ là màu trắng
+    ucg_SetColor(&ucg, 1, 0, 0, 0);           // màu background là màu đen
+    ucg_SetRotate180(&ucg);                   // quay màn hình lại 180 độ
+    ucg_DrawString(&ucg, 0, 12, 0, "I love Embedded"); // in dòng chữ ra LCD
+    ucg_DrawString(&ucg, 0, 26, 0, "Programming");
+
     /* Loop forever */
-	for(;;);
+    while(1)
+    {
+        processTimerScheduler();
+    }
 }
